ea319b0461710e1c712e1f91460b21df1134ba7a
SafaryanAlexandr <11220099.as@gmail.com>
2019-10-28 23:12:07 +0300

    task 1.2.1 v2.1

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -1,6 +1,32 @@
 <?php
 require "form.html";
-$test = $_POST["login"];
-if(!preg_match("/\w/",$test)) {
-    echo ("Логин $test содержит недопустимый символ!");
+$login = $_POST["login"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$name = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$middleName = $_POST["middleName"];
+$code = $_POST["code"];
+
+if(!preg_match("/\w/",$login)) {
+    echo "Логин $login содержит недопустимый символ!<br>";
+}
+if(!preg_match("/\d{8,15}/",$password)) {
+    echo "Пароль должен быть от 8 до 15 символов!<br>";
+}
+if(!preg_match("/\w{1}@\w.\w/",$email)) {
+    echo"Почта должна быть формата почта@домен.доменнаязона.<br>";
+}
+if(!preg_match("/\w+/",$name)) {
+    echo "Введите ИМЯ <br>";
+}
+if(!preg_match("/\w+/",$lastName)) {
+    echo "Введите ФАМИЛИЮ <br>";
+}
+if(!preg_match("/\w+/",$middleName)) {
+    echo "Введите ОТЧЕСТВО <br>";
+}
+
+if (!$code === "nd82jaake") {
+  echo "Кодовое слово не верное!!! <br>";
 }
