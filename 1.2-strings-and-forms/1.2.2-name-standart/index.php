50f8370ab3797e304fad82f3115690ae68e0d316
SafaryanAlexandr <11220099.as@gmail.com>
2019-11-04 23:03:30 +0300

    task 1.2.2 v2

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -3,28 +3,34 @@ require "form.html";
 $firstName = $_POST["firstName"];
 $lastName = $_POST["lastName"];
 $middleName = $_POST["middleName"];
-if(preg_match("/[A-z]/",$firstName)) {
-    $firstName = ucfirst($firstName);
+if(preg_match("/[А-я]/",$firstName)) {
+    $firstName = mb_strtolower($firstName);
+    $firstName0 = mb_strtoupper(mb_substr($firstName,0,1));
+    $firstName = $firstName0 . mb_substr($firstName,1,(mb_strlen($firstName) - 1));
 } else {
   $number = 1;
 }
-if(preg_match("/[A-z]/",$lastName)) {
-    $lastName = ucfirst($lastName);
+if(preg_match("/[А-я]/",$lastName)) {
+    $lastName = mb_strtolower($lastName);
+    $lastName0 = mb_strtoupper(mb_substr($lastName,0,1));
+    $lastName = $lastName0 . mb_substr($lastName,1,(mb_strlen($lastName) - 1));
 } else {
   $number = 1;
 }
-if(preg_match("/[A-z]/",$middleName)) {
-    $middleName = ucfirst($middleName);
+if(preg_match("/[А-я]/",$middleName)) {
+    $middleName = mb_strtolower($middleName);
+    $middleName0 = mb_strtoupper(mb_substr($middleName,0,1));
+    $middleName = $middleName0 . mb_substr($middleName,1,(mb_strlen($middleName) - 1));
 } else {
   $number = 1;
 }
 if ($number === 1) {
-  echo "Используйте символы A-z.";
+  echo "Используйте символы А-я.";
 } else {
   $fullName = "$firstName $lastName $middleName<br>";
   echo $fullName;
-  $fio = $firstName{0} . $lastName{0}. $middleName{0};
+  $fio = $firstName0 . $lastName0 . $middleName0;
   echo "$fio<br>";
-  $surnameAndInitials = $lastName . " " . $firstName{0} . ". " .  $middleName{0} .".";
-   echo $surnameAndInitials;
+  $surnameAndInitials = $lastName . " " . $firstName0 . ". " .  $middleName0 .".";
+  echo $surnameAndInitials;
 }
